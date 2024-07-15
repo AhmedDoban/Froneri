136718b047c89966b68b9acbba52ce5974ae4203
Ahmed Dobban <73547094+AhmedDoban@users.noreply.github.com>
2024-07-15 11:43:48 +0300

    update Our programs

--- src/app/Home/Home.jsx
+++ src/app/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Landing from "./Landing/Landing";
+import OurPrograms from "./OurPrograms/OurPrograms";
 
 function Home() {
   return (
     <div className="Home">
       <Landing />
+      <OurPrograms />
     </div>
   );
 }
---
+++ src/app/Home/OurPrograms/OurPrograms.jsx
@@ -0,0 +1,78 @@
+import Image from "next/image";
+import "./OurPrograms.css";
+
+function OurPrograms() {
+  return (
+    <div className="OurPrograms">
+      <div className="OurPrograms_Cicle">
+        <div className="container">
+          <div className="info" data-aos="fade-down">
+            <Image src="/logo.svg" width={100} height={30} alt="Froneri" />
+            <h2>Our programs</h2>
+            <p>
+              you'll have the chance to work on cutting-edge projects that shape
+              the future of technology, while continuously honing your skills
+              and expertise
+            </p>
+          </div>
+          <div className="Programs_Container">
+            <div className="box" data-aos="zoom-in">
+              <Image
+                src="/StudentPrograms.jpeg"
+                width={500}
+                height={300}
+                alt="Student Programs"
+              />
+              <h3>Student Programs</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur. Vulputate feugiat dictum
+                ut a sed dapibus non nullam. Cras massa aliquet vitae neque
+                libero. Nibh gravida cursus dictumst tortor. Est diam sed netus
+                laoreet suspendisse amet augue a. Vestibulum turpis tellus lorem
+                adipiscing. Elementum et neque quam dignissim aliquet. Magna
+                aliquet vitae ultricies et nisi ac suspendisse. Pulvinar
+                porttitor neque amet nunc fames gravida in facilisis. Curabitur
+                neque libero aliquet
+              </p>
+              <button>
+                <span>Learn more</span>
+                <Image
+                  src="/icons/Arrow-right.svg"
+                  width={25}
+                  height={25}
+                  alt="arrow-right"
+                />
+              </button>
+            </div>
+            <div className="box" data-aos="zoom-in">
+              <Image
+                src="/FreshGraduatesPrograms.jpeg"
+                width={500}
+                height={200}
+                alt="Student Programs"
+              />
+              <h3>Fresh Graduates Programs</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur. Porttitor non mi risus
+                aenean nunc cursus est. Viverra nisl venenatis purus lectus.
+                Feugiat malesuada a molestie morbi turpis arcu mauris tincidunt.
+                Gravida lobortis eu at tempus bibendum tristique nisi.
+                Pellentesque nunc vitae condimentum lectus vel adipiscing.
+              </p>
+              <button>
+                <span>Learn more</span>
+                <Image
+                  src="/icons/Arrow-right.svg"
+                  width={25}
+                  height={25}
+                  alt="arrow-right"
+                />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default OurPrograms;
