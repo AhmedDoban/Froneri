824fab6e6fdaafc3a50aa4446a3cfc78089485d2
Ahmed Dobban <73547094+AhmedDoban@users.noreply.github.com>
2024-07-16 14:54:08 +0300

    update   .Graduates Landing

---
+++ src/app/Fresh_Graduates_Programs/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Landing from "./Landing/Landing";
+
+function Fresh_Graduates_Programs() {
+  return (
+    <div className="Fresh_Graduates_Programs">
+      <Landing />
+    </div>
+  );
+}
+export default Fresh_Graduates_Programs;
